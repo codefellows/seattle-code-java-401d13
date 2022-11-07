68d641d365ada5d0ed07be87b2a7333d70fbfeff
Instructor Alex White <alex.white@codefellows.com>
2022-11-07 13:02:21 -0800

    class 11 demo

---
+++ class-11/class11Demo/src/main/java/com/cyberZork/class11Demo/Class11DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cyberZork.class11Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class11DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class11DemoApplication.class, args);
+	}
+
+}
---
+++ class-11/class11Demo/src/main/java/com/cyberZork/class11Demo/albums/Album.java
@@ -0,0 +1,6 @@
+package com.cyberZork.class11Demo.albums;
+
+
+public class Album {
+  private String name;
+}
