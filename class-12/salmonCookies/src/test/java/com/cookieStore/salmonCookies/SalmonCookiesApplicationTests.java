6be48d65981ee1c94b2e6ab018d1901dd7a10293
Instructor Alex White <alex.white@codefellows.com>
2022-11-08 13:26:15 -0800

    class 12 demo

---
+++ class-12/salmonCookies/src/test/java/com/cookieStore/salmonCookies/SalmonCookiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookieStore.salmonCookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalmonCookiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
