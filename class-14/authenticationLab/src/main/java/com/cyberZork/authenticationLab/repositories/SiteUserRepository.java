a002efc60da86ea1f8e1fbd81a1dd499321eea26
Joe Rutkin <107508558+rutkinj@users.noreply.github.com>
2022-11-10 17:09:53 -0800

    adds comments

--- class-14/authenticationLab/src/main/java/com/cyberZork/authenticationLab/repositories/SiteUserRepository.java
+++ class-14/authenticationLab/src/main/java/com/cyberZork/authenticationLab/repositories/SiteUserRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+// This is where some postgres black magic is occuring
+// jpa repository does a bunch of work to map objects to tables behind the scenes
 public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
+  // jpa repository has many built in methods
+  // but if you want any that are specific to your object you write them here
   public SiteUser getSiteUserByUserName(String UserName);
 
 }
