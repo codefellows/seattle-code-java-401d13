ec1deaea04256136c37bafef4f3b731c344f1a56
Armasconi <armasconi@Armasconis-MacBook-Pro.local>
2022-11-10 17:46:58 -0800

    commented-by-aa-whatabouttests

--- class-14/authenticationLab/src/main/java/com/cyberZork/authenticationLab/repositories/SiteUserRepository.java
+++ class-14/authenticationLab/src/main/java/com/cyberZork/authenticationLab/repositories/SiteUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
-  public SiteUser getSiteUserByUserName(String UserName); //custom method for finding proper user
-
+  public SiteUser getSiteUserByUserName(String UserName); //custom method for finding proper user, yeah-AA
+//wondering about the clunky name, i know it has to be a certain way, SiteUser + By + UserName ?-AA
 }
