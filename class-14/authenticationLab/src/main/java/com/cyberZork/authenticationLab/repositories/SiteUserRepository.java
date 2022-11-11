25f00f84dbefb0e7812ce150d239c06d0365fe77
Armasconi <armasconi@Armasconis-MacBook-Pro.local>
2022-11-10 17:42:18 -0800

    commented-by-aa

--- class-14/authenticationLab/src/main/java/com/cyberZork/authenticationLab/repositories/SiteUserRepository.java
+++ class-14/authenticationLab/src/main/java/com/cyberZork/authenticationLab/repositories/SiteUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
-  public SiteUser getSiteUserByUserName(String UserName);
+  public SiteUser getSiteUserByUserName(String UserName); //custom method for finding proper user
 
 }
