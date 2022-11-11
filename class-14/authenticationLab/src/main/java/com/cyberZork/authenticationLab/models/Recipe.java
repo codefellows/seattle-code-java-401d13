6db2bac4704a76819a07ba48fd2cb594db688ada
Joe Rutkin <107508558+rutkinj@users.noreply.github.com>
2022-11-10 17:06:13 -0800

    adds comments

--- class-14/authenticationLab/src/main/java/com/cyberZork/authenticationLab/models/Recipe.java
+++ class-14/authenticationLab/src/main/java/com/cyberZork/authenticationLab/models/Recipe.java
@@ -2,8 +2,10 @@ package com.cyberZork.authenticationLab.models;
 
 import javax.persistence.*;
 
+// entity, as I understand it, makes the class available to the repository interface
 @Entity
 public class Recipe {
+  // this is how you tell postgres to generate an id for an instance of this object when it's put into the database
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
