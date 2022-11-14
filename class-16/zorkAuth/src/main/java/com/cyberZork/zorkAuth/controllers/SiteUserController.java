3145d0734265cd51834729fc1d59c267b852c856
Instructor Alex White <alex.white@codefellows.com>
2022-11-14 14:42:15 -0800

    added logout and auto login functionality

--- class-16/zorkAuth/src/main/java/com/cyberZork/zorkAuth/controllers/SiteUserController.java
+++ class-16/zorkAuth/src/main/java/com/cyberZork/zorkAuth/controllers/SiteUserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.Date;
 
@@ -21,6 +23,9 @@ public class SiteUserController {
 
   @Autowired
   PasswordEncoder passwordEncoder;
+
+  @Autowired
+  HttpServletRequest request;
 
   // homepage
     // Anybody? YES\
@@ -68,9 +73,17 @@ public class SiteUserController {
     // save the user
     siteUserRepo.save(newUser);
     // auto login -> httpServletRequest
+    authWithHttpServletRequest(username, password);
     return new RedirectView("/");
   }
 
+  public void authWithHttpServletRequest(String username, String password){
+    try {
+      request.login(username, password);
+    } catch (ServletException e) {
+      e.printStackTrace();
+    }
+  }
 
 
   // logout
