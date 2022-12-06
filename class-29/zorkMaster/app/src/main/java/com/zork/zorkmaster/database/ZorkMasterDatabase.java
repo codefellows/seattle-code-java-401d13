25ee04d43ad421eb2958a97bc20341d204cf06d5
Instructor Alex White <alex.white@codefellows.com>
2022-12-06 13:25:02 -0800

    class 29 demo

---
+++ class-29/zorkMaster/app/src/main/java/com/zork/zorkmaster/database/ZorkMasterDatabase.java
@@ -0,0 +1,17 @@
+package com.zork.zorkmaster.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import com.zork.zorkmaster.dao.SuperPetDao;
+import com.zork.zorkmaster.models.SuperPet;
+
+// TODO Step: 4-6 Enable converters
+@TypeConverters({ZorkMasterDatabaseConverters.class})
+//TODO Step: 4-5 Setup the database!
+@Database(entities = {SuperPet.class}, version = 1) // if we update the version, it will delete the db!
+public abstract class ZorkMasterDatabase extends RoomDatabase {
+  // TODO Step: 4-6 add your DAO's!
+  public abstract SuperPetDao superPetDao();
+}
