710b77459c6a28e5ae6c4a07d09f2c4c309a1d17
Instructor Alex White <alex.white@codefellows.com>
2022-12-08 12:21:32 -0800

    class 32 demo

--- class-31/zorkMaster/app/src/main/java/com/zork/zorkmaster/adapter/SuperPetRecyclerViewAdapter.java
+++ class-31/zorkMaster/app/src/main/java/com/zork/zorkmaster/adapter/SuperPetRecyclerViewAdapter.java
@@ -10,17 +10,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.amplifyframework.datastore.generated.model.SuperPet;
 import com.zork.zorkmaster.R;
 import com.zork.zorkmaster.activities.OrderForm;
 import com.zork.zorkmaster.activities.SuperPetActivity;
-import com.zork.zorkmaster.models.SuperPet;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
-// TODO Step 1-4: Make a class whose sole purpose is to manage RecyclerViews: a RecyclerView.Adapter
-  // TODO Step 3-1: (In RecyclerViewAdapter) Clean up the RecyclerView.Adapter references to actually use SuperPetRecyclerViewAdapter
 public class SuperPetRecyclerViewAdapter extends RecyclerView.Adapter<SuperPetRecyclerViewAdapter.SuperPetViewHolder> {
-  // TODO Step 2-3: (In this activity and RecyclerViewAdapter) Hand in some data items
   List<SuperPet> superPets;
   Context callingActivity;
 
@@ -32,23 +34,20 @@ public class SuperPetRecyclerViewAdapter extends RecyclerView.Adapter<SuperPetRe
   @NonNull
   @Override
   public SuperPetViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    // TODO Step 1-7: (In RecyclerViewAdapter.onCreateViewHolder()) Inflate fragment
     View superPetFragment = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_super_pet, parent, false);
-    // TODO Step 1-9: (In RecyclerViewAdapter.onCreateViewHolder()) Attach Fragment to ViewHolder
+
     return new SuperPetViewHolder(superPetFragment);
   }
 
   @Override
   public void onBindViewHolder(@NonNull SuperPetViewHolder holder, int position) {
-    // TODO Step 2-4: (In RecyclerViewAdapter.onBindViewHolder()) Bind data items to Fragments inside of ViewHolders
     TextView superPetFragmentTextViewName = holder.itemView.findViewById(R.id.SuperPetFragTVName);
-    // TODO Step 6-2 refactor the rendering
     SuperPet superPet = superPets.get(position);
+
     superPetFragmentTextViewName.setText((position+1) + ". " + superPet.getName()
       + "\n" + superPet.getHeight()
       + "\n" + superPet.getType()
       + "\n" + superPet.getBirthDate());
-    // TODO Step 3-3: (In RecyclerViewAdapter.onBindViewHolder()) Create OnClickListener, make an Intent inside it, and call this Intent with an Extra to go to another Activity
     View superPetItemView = holder.itemView;
     superPetItemView.setOnClickListener(v -> {
       Intent goToOrderFormIntent = new Intent(callingActivity, OrderForm.class);
@@ -57,15 +56,12 @@ public class SuperPetRecyclerViewAdapter extends RecyclerView.Adapter<SuperPetRe
     });
   }
 
+
   @Override
   public int getItemCount() {
-    // TODO Step 1-10: (In RecyclerViewAdapter.getItemCount()) For testing purposes, hardcode a large number of items
-//    return 100;
-  // TODO Step 2-5: (In RecyclerViewAdapter.getItemCount()) Make the size of the list dynamic
     return superPets.size();
   }
 
-  // TODO Step 1-8: (In RecyclerViewAdapter) Make a ViewHolder class to hold a Fragment
     public static class SuperPetViewHolder extends RecyclerView.ViewHolder{
       public SuperPetViewHolder(@NonNull View itemView) {
         super(itemView);
